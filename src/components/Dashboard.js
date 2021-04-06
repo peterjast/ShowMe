c91c57e5c2c1fa579d9b39bf2b6d591ed2ca008a
Peter Staker <peterjast@gmail.com>
2021-04-06 09:03:29 -0700

    debugging

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,23 +10,27 @@ class Dashboard extends React.Component {
         super(props);
         this.state={
             displaySearchResults: false,
+            displayTrending: true,
             searchResults: [],
             searchQuery: ''
         }
     }
 
     getSearchResults = async(e) => {
+        e.preventDefault();
         try{
             const SERVER = process.env.REACT_APP_SERVER;
 
             const media = await axios.get(`${SERVER}/search`, { params: { searchQuery: this.state.searchQuery }});
-            const searchResults = media.data;
+            const searchResults = media.data.movieArray;
 
             this.setState({
                 displaySearchResults: true,
                 displayTrending: false, 
                 searchResults: searchResults
             });
+
+            console.log('searchResults:', searchResults);
         } catch(err) {
             console.log(err);
         } 
@@ -36,7 +40,7 @@ class Dashboard extends React.Component {
         return(
             <>
                 <form className="w-50 p3 mx-auto mb-5" onSubmit={this.getSearchResults} >
-                    <input className="w-75 p3" onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="city"/>
+                    <input className="w-75 p3" onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="find movies or shows"/>
                     <button className="w-25 p3" type="submit">Search</button>
                 </form>
                 {!this.state.displaySearchResults ?
@@ -44,7 +48,7 @@ class Dashboard extends React.Component {
                 :
                 this.state.searchResults.map((mediaObj, index) => (
                     mediaObj.poster_path &&
-                    <Card key={mediaObj.id} className="mx-auto mb-5 overflow-auto" style={{ width: '20rem', height: '28rem'}} bg="dark" text="light">
+                    <Card key={index} className="mx-auto mb-5 overflow-auto" style={{ width: '20rem', height: '28rem'}} bg="dark" text="light">
                     <Card.Img variant="top" alt={mediaObj.title} src={`https://www.themoviedb.org/t/p/original${mediaObj.poster_path}`} />
                     <Card.Body>
                         <Card.Title>{mediaObj.title}</Card.Title>
